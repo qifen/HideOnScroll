771c95efb10e765a92be78a8e64766fce176cb4a
qifen <547799503@qq.com>
2016-11-22 18:36:06 +0800

    first commit

---
+++ app/src/main/java/com/wei/hideonscroll/RecyclerItemViewHolder.java
@@ -0,0 +1,29 @@
+package com.wei.hideonscroll;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by weiyilin on 16/11/18.
+ */
+
+public class RecyclerItemViewHolder extends RecyclerView.ViewHolder {
+
+    private final TextView mItemTextView;
+
+    public RecyclerItemViewHolder(final View parent, TextView itemTextView) {
+        super(parent);
+        mItemTextView = itemTextView;
+    }
+
+    public static RecyclerItemViewHolder newInstance(View parent) {
+        TextView itemTextView = (TextView) parent.findViewById(R.id.itemTextView);
+        return new RecyclerItemViewHolder(parent, itemTextView);
+    }
+
+    public void setItemText(CharSequence text) {
+        mItemTextView.setText(text);
+    }
+
+}
---
+++ app/src/main/java/com/wei/hideonscroll/Utils.java
@@ -0,0 +1,24 @@
+package com.wei.hideonscroll;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+
+/**
+ * Created by weiyilin on 16/11/18.
+ */
+
+public class Utils {
+
+    public static int getToolbarHeight(Context context) {
+        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                new int[]{R.attr.actionBarSize});
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        return toolbarHeight;
+    }
+
+    public static int getTabsHeight(Context context) {
+        return (int) context.getResources().getDimension(R.dimen.tabsHeight);
+    }
+}
